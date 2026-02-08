574a016e07d019be3d936e6007f2e3b5d569c69c
nagaraju274 <naagalways@gmail.com>
2026-02-08 12:03:37 +0530

    converting int data type to other data types

---
+++ 1_python_basics/day_1/convertion_of_data_types.py
@@ -0,0 +1,14 @@
+#______________________________________________________________________________________
+#converting int data type to other data types
+a=5
+print(a) #5
+print(type(a))#<type 'int'>
+print(type(float(a)))#<type 'float'>
+print(type(str(a)))#<type 'str'>
+print(type(bool(a)))#<type 'bool'>
+print(type(complex(a)))#<type 'complex'>
+#--------------------------------------------------------------------------------------
+
+
+
+
--- 1_python_basics/day_1/trail.py
+++
@@ -1,5 +0,0 @@
-a=5
-print(a)
-print(type(a))
-b=bool(a)
-print()
