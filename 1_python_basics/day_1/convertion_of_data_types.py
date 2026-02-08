dcaf57cbd3c48f61d329a1552cfb923d9fcfbfdb
nagaraju274 <naagalways@gmail.com>
2026-02-08 12:08:55 +0530

    converting float data type to other data types

--- 1_python_basics/day_1/convertion_of_data_types.py
+++ 1_python_basics/day_1/convertion_of_data_types.py
@@ -8,7 +8,15 @@ print(type(str(a)))#<type 'str'>
 print(type(bool(a)))#<type 'bool'>
 print(type(complex(a)))#<type 'complex'>
 #--------------------------------------------------------------------------------------
-
+#--------------------------------------------------------------------------------------
+#converting float data type to other data types
+b=5.5
+print(b) #5.5
+print(type(b))#<type 'float'>
+print(type(int(b)))#<type 'int'>
+print(type(str(b)))#<type 'str'>
+print(type(bool(b)))#<type 'bool'>
+print(type(complex(b)))#<type 'complex'>
 
 
 
